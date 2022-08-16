1ab3f37b235c69bb8053643af1e358786f365761
Neighbouring <neighbouringco@gmail.com>
2022-08-16 00:04:53 -0300

    jQuery

--- assets/_jquery_auto_slider.js
+++ assets/_jquery_auto_slider.js
@@ -5,15 +5,17 @@
 //   console.log("JQUERY PARAR");
 // });
 
-$(".anima-auto-play").ready(function(){
+$(".anima-auto-play").hover(function(){
+  console.log("CAMADA 1");
   $(".anima-auto-play").hover(function(){
+    console.log("CAMADA 2");
     $(".anima-auto-play").toggleClass('.parar');
   });
 });
 
-$('.anima-auto-play').blur(function () {
-  $('.anima-auto-play').toggleClass('.rodar');
-  console.log("JQUERY RODAR");
-});
+// $('.anima-auto-play').blur(function () {
+//   $('.anima-auto-play').toggleClass('.rodar');
+//   console.log("JQUERY RODAR");
+// });
 
 console.log("JQUERY ATIVO");
