0b4df5f25b457fa0403e32aca7b5c9c2ec7c7318
Neighbouring <neighbouringco@gmail.com>
2022-08-15 23:48:28 -0300

    jQuery Script

--- assets/_jquery_auto_slider.js
+++ assets/_jquery_auto_slider.js
@@ -1,11 +1,11 @@
-$('#section-auto-play').hide();
+// $('#section-auto-play').hide();
 
 $('#section-auto-play').hover(function () {
   $('#section-auto-play').toggleClass('parar');
 });
 
-$('#section-auto-play').blur(function () {
-  $('#section-auto-play').toggleClass('rodar');
-});
+// $('#section-auto-play').blur(function () {
+//   $('#section-auto-play').toggleClass('rodar');
+// });
 
 console.log("JQUERY ATIVO");
