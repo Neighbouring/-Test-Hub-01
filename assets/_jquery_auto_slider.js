76033d780ca3285477d8d94f97281c5445af7002
Neighbouring <neighbouringco@gmail.com>
2022-08-16 00:22:22 -0300

    jQuery - Auto Slider

--- assets/_jquery_auto_slider.js
+++ assets/_jquery_auto_slider.js
@@ -1,13 +1,7 @@
-// $('#section-auto-play').hide();
-
 $("article.anima-auto-play").hover(function () {
   $(".anima-auto-play").toggleClass("parar");
-  console.log("JQUERY PARAR");
 });
 
 $('article.anima-auto-play').blur(function () {
   $('.anima-auto-play').toggleClass('rodar');
-  console.log("JQUERY RODAR");
 });
-
-console.log("JQUERY ATIVO");
